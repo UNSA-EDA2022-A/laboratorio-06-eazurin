1f664197e622b29562c735646390511060cac228
Eberth <88208531+eazurin@users.noreply.github.com>
2022-07-06 19:27:43 -0500

    Update Exercise3.java

--- src/main/java/com/example/project/Exercise3.java
+++ src/main/java/com/example/project/Exercise3.java
@@ -18,8 +18,23 @@ public class Exercise3 {
     }
 
 
-    public <T extends Comparable<T>> boolean bstEstrictamenteBinario(BST<T> a){
+     public <T extends Comparable<T>> boolean bstEstrictamenteBinario(BST<T> a){
+    	Node<T> aux = a.root; //extraemos la raiz para acceder a los nodos
+    	if(a.isEmpty()) {
+    		return true; //si esta vacio asumiremos que es binario
+    	}
+    	return nodeBinario(aux); //llamamos al metodo recursivo
 
-        return false;
+    }
+    private <T extends Comparable<T>> boolean nodeBinario(Node<T> a){
+    	if(a.right != null && a.left != null) { //Si sus hijos son diferente de nulo bajaremos de nivel
+    		return nodeBinario(a.left) && nodeBinario(a.right); //si ambos hijos son perfectamente binarios el arbol sera estrictamente binario
+    	}
+    	else if((a.right != null && a.left == null) || (a.right == null && a.left != null)) { //si un nodo hijo es nulo y el otro no, no sera 
+    		//estrictamente binario
+    		return false;
+    	}
+    	else //si ambos hijos son nulos sera estrictammente binario
+    		return true;
     }
 }
